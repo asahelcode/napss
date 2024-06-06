9758f6c95f13ffbbfeade06423c908f74c656c00
c0deMercenary <hoyx0101@gmail.com>
2024-06-06 21:58:25 +0100

    fixes done

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
 
 const client = new ApolloClient({
-  // uri: 'https://napss-server.onrender.com/graphql',
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://napss-server.onrender.com/graphql',
+  // uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache()
 })
 ReactDOM.createRoot(document.getElementById('root')!).render(
